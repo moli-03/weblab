635fca8ba9a85fc6fd2dab76353cb0842e5d7a32
Venetz Robin <venetz.robin@gmail.com>
2025-09-02 22:27:01 +0200

    refined db model and added temporary register endpoint

---
+++ server/api/auth/register.post.ts
@@ -0,0 +1,48 @@
+import { eq } from "drizzle-orm";
+import z from "zod";
+import { db } from "~~/server/database";
+import { users } from "~~/server/database/schema";
+import { hashPassword } from "~~/server/security/password";
+
+const bodySchema = z.object({
+  email: z.email().refine(
+    async email => {
+      const existingUser = await db.query.users.findFirst({
+        where: eq(users.email, email),
+      });
+      return !existingUser;
+    },
+    {
+      error: "Email already in use",
+    },
+  ),
+  password: z.string().min(8),
+  name: z.string().min(2).max(255),
+});
+
+export default defineEventHandler(async event => {
+  const { email, password, name } = await readValidatedBody(event, bodySchema.parseAsync);
+
+  try {
+    const user = await db
+      .insert(users)
+      .values({
+        email,
+        name,
+        passwordHash: await hashPassword(password),
+        roles: ["customer"],
+      })
+      .returning();
+
+    return {
+      user,
+    };
+  } catch (error) {
+    console.error("Error registering user:", error);
+    throw createError({
+      statusCode: 500,
+      statusMessage: "Internal Server Error",
+      message: "Failed to register user",
+    });
+  }
+});
--- server/database/schema.ts
+++ server/database/schema.ts
@@ -1,14 +1,98 @@
-import { json, pgTable, uuid, timestamp, varchar } from "drizzle-orm/pg-core";
+import { relations, sql } from "drizzle-orm";
+import { json, pgTable, uuid, timestamp, varchar,  primaryKey, uniqueIndex, pgEnum, text, check, inet, boolean } from "drizzle-orm/pg-core";
+
+export const technologyCategory = pgEnum("technology_category", ["technique", "tool", "platform", "framework"]);
+export const technologyRing = pgEnum("technology_ring", ["adopt", "trial", "assess", "hold"]);
+export const technologyStatus = pgEnum("technology_status", ["draft", "published"]);
 
 export const users = pgTable("users", {
   id: uuid("id").primaryKey().defaultRandom(),
   name: varchar("last_name", { length: 255 }).notNull(),
-  email: varchar("email", { length: 255 }).notNull().unique(),
+  email: varchar("email", { length: 255 }).notNull(),
   passwordHash: varchar("password_hash", { length: 255 }).notNull(),
   roles: json("roles").notNull().$type<Array<"admin" | "cto" | "customer">>(),
-  createdAt: timestamp("created_at").defaultNow(),
-  updatedAt: timestamp("updated_at").defaultNow(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull()
+}, table => [
+    uniqueIndex("email_idx").on(table.email)
+]);
+
+export const loginAudit = pgTable("login_audit", {
+    id: uuid("id").primaryKey().defaultRandom(),
+    userId: uuid("user_id").notNull().references(() => users.id),
+    attemptedAt: timestamp("attempted_at").defaultNow().notNull(),
+    ipAddress: inet("ip_address").notNull(),
+    userAgent: text("user_agent").notNull(),
+    loginSuccessful: boolean("login_successful").notNull(),
+    failureReason: varchar("failure_reason", { length: 255 })
 });
 
+export const workspaces = pgTable("workspaces", {
+    id: uuid("id").primaryKey().defaultRandom(),
+    name: varchar("name", { length: 255 }).notNull(),
+    createdAt: timestamp("created_at").defaultNow(),
+    updatedAt: timestamp("updated_at").defaultNow(),
+});
+
+export const userWorkspaces = pgTable("user_workspaces", {
+    userId: uuid("user_id").notNull().references(() => users.id),
+    workspaceId: uuid("workspace_id").notNull().references(() => workspaces.id),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").defaultNow().notNull()
+}, table => [
+    primaryKey({ columns: [table.userId, table.workspaceId] }),
+]);
+
+export const technology = pgTable("technology", {
+    id: uuid("id").primaryKey().defaultRandom(),
+    workspaceId: uuid("workspace_id").notNull().references(() => workspaces.id),
+    name: varchar("name", { length: 255 }).notNull(),
+    category: technologyCategory("category").notNull(),
+    description: text("description").notNull(),
+    ring: technologyRing("ring"),
+    ringDescription: text("ring_description"),
+    status: technologyStatus("status").notNull(),
+    publishedAt: timestamp("published_at"),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").defaultNow().notNull()
+}, table => [
+    // Unique technologies per workspace
+    uniqueIndex("workspace_technology_idx").on(table.workspaceId, table.name),
+    check("published_tech_must_have_ring", sql`
+        (${table.status} = "draft") 
+        OR (${table.status} = "published" AND ${table.ring} IS NOT NULL AND ${table.publishedAt} IS NOT NULL
+    )`)
+]);
+
+
+
+// Relations
+export const userRelations = relations(users, ({ many }) => ({
+    userWorkspaces: many(userWorkspaces),
+    loginAudits: many(loginAudit),
+}));
+
+export const workspaceRelations = relations(workspaces, ({ many }) => ({
+    userWorkspaces: many(userWorkspaces),
+}));
+
+export const userWorkspaceRelations = relations(userWorkspaces, ({ one }) => ({
+    user: one(users, {
+        fields: [userWorkspaces.userId],
+        references: [users.id],
+    }),
+    workspace: one(workspaces, {
+        fields: [userWorkspaces.workspaceId],
+        references: [workspaces.id],
+    }),
+}));
+
+// Types
 export type User = typeof users.$inferSelect;
 export type NewUser = typeof users.$inferInsert;
+export type Workspace = typeof workspaces.$inferSelect;
+export type NewWorkspace = typeof workspaces.$inferInsert;
+export type UserWorkspace = typeof userWorkspaces.$inferSelect;
+export type NewUserWorkspace = typeof userWorkspaces.$inferInsert;
+export type Technology = typeof technology.$inferSelect;
+export type NewTechnology = typeof technology.$inferInsert;
