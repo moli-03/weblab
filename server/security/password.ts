ed07bf392ca7cdc3c9e73a3c205c62a02a61f392
Venetz Robin <venetz.robin@gmail.com>
2025-09-03 15:32:01 +0200

    fixes

--- server/security/password.ts
+++ server/security/password.ts
@@ -1,23 +1,13 @@
 import bcrypt from "bcrypt";
 
-// Use a higher salt rounds for better security (12 is more secure than 10)
 const SALT_ROUNDS = 12;
 
 export async function hashPassword(password: string): Promise<string> {
-  // Validate password strength
-  if (password.length < 8) {
-    throw new Error("Password must be at least 8 characters long");
-  }
-
   const salt = await bcrypt.genSalt(SALT_ROUNDS);
   return bcrypt.hash(password, salt);
 }
 
 export async function verifyPassword(password: string, hashed: string): Promise<boolean> {
-  if (!password || !hashed) {
-    return false;
-  }
-
   try {
     return await bcrypt.compare(password, hashed);
   } catch (error) {
